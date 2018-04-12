15e72b7fa3758247896c736b4f45a8cfc8214ffd
carlos.zarate <zaratec31@hotmail.com>
2018-04-12 14:17:02 -0500

    Colcando CalendarView

--- app/src/main/java/com/example/carloszarate/proyectomenu/fragment/LocationFragment.java
+++ app/src/main/java/com/example/carloszarate/proyectomenu/fragment/LocationFragment.java
@@ -8,11 +8,79 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.carloszarate.proyectomenu.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class LocationFragment extends Fragment {
+    MapView mapView;
+    private GoogleMap googleMap;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_location,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_location,container,false);
+        mapView = (MapView) rootView.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume(); // obtener el mapa y mostrarlo inmediatamente
+
+        try{
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+        }
+
+
+
+        double latitude = 17.385044;
+        double longitude = 78.486671;
+
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Hello Maps");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        // adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(17.385044, 78.486671)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+        return rootView;
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
